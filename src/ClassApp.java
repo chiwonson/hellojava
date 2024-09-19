560b9d0f1a84667c2e523b253f3f3cc2fb7db7be
test <chewonson@chewons-MacBook-Air.local>
2024-09-19 23:20:58 +0900

    2024.09.19.commit

---
+++ src/ClassApp.java
@@ -0,0 +1,13 @@
+import java.io.PrintWriter;
+
+public class ClassApp {
+
+    public static void main (String[] args){
+
+
+
+        System.out.println(Math.PI);
+
+
+    }
+}
---
+++ src/Datatype.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Datatype {
+    public static void main(String[] args){
+        System.out.println(6); // Number
+        System.out.println("6"); // String
+
+        double d = 1.1;
+        System.out.println(d);
+
+        String id = JOptionPane.showInputDialog("Enter a id");
+    }
+}
